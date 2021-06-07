026fd136baf3bc61d7299abfaa77390c2dd1a0e5
Fred Gleason <fredg@paravelsystems.com>
2021-06-07 15:24:14 -0400

    2021-06-07 Fred Gleason <fredg@paravelsystems.com> 	* Created branch 'rhel_7'. 	* Populated initial assets and scripts.
    Signed-off-by: Fred Gleason <fredg@paravelsystems.com>

---
+++ assets/installer_install_rivendell.sh
@@ -0,0 +1,259 @@
+#!/bin/sh
+
+# installer_install_rivendell.sh
+#
+# Install Rivendell 4.x on a CentOS 7 system
+#
+
+#
+# Site Defines
+#
+REPO_HOSTNAME="software.paravelsystems.com"
+
+#
+# Get Target Mode
+#
+if test $1 ; then
+    case "$1" in
+	--client)
+	    MODE="client"
+	    IP_ADDR=$2
+	    ;;
+
+	--server)
+	    MODE="server"
+	    ;;
+
+	--standalone)
+	    MODE="standalone"
+	    ;;
+
+	*)
+	    echo "USAGE: ./install_rivendell.sh --client|--server|--standalone"
+	    exit 256
+            ;;
+    esac
+else
+    MODE="standalone"
+fi
+
+#
+# Configure Repos
+#
+yum -y install epel-release
+wget http://$REPO_HOSTNAME/CentOS/7com/Paravel-Commercial.repo -P /etc/yum.repos.d/
+
+#
+# Install XFCE4
+#
+yum -y groupinstall "X window system"
+yum -y groupinstall xfce
+systemctl set-default graphical.target
+
+#
+# Install Dependencies
+#
+yum -y install patch evince telnet lwmon nc samba paravelview ntp emacs twolame libmad nfs-utils cifs-utils samba-client ssvnc xfce4-screenshooter net-tools alsa-utils cups tigervnc-server-minimal pygtk2 cups system-config-printer gedit ntfs-3g ntfsprogs autofs
+
+if test $MODE = "server" ; then
+    #
+    # Install MariaDB
+    #
+    yum -y install mariadb-server
+    systemctl start mariadb
+    systemctl enable mariadb
+    mkdir -p /etc/systemd/system/mariadb.service.d/
+    cp /usr/share/rivendell-install/limits.conf /etc/systemd/system/mariadb.service.d/
+    systemctl daemon-reload
+
+    #
+    # Enable DB Access for localhost
+    #
+    echo "CREATE DATABASE Rivendell;" | mysql -u root
+    echo "CREATE USER 'rduser'@'localhost' IDENTIFIED BY 'letmein';" | mysql -u root
+    echo "GRANT SELECT,INSERT,UPDATE,DELETE,CREATE,DROP,INDEX,ALTER,CREATE TEMPORARY TABLES,LOCK TABLES ON Rivendell.* TO 'rduser'@'localhost';" | mysql -u root
+
+    #
+    # Enable DB Access for all remote hosts
+    #
+    echo "CREATE USER 'rduser'@'%' IDENTIFIED BY 'letmein';" | mysql -u root
+    echo "GRANT SELECT,INSERT,UPDATE,DELETE,CREATE,DROP,INDEX,ALTER,CREATE TEMPORARY TABLES,LOCK TABLES ON Rivendell.* TO 'rduser'@'%';" | mysql -u root
+
+    #
+    # Enable NFS Access for all remote hosts
+    #
+    echo "/var/snd *(rw,no_root_squash)" >> /etc/exports
+    echo "/home/rd/rd_xfer *(rw,no_root_squash)" >> /etc/exports
+    echo "/home/rd/music_export *(rw,no_root_squash)" >> /etc/exports
+    echo "/home/rd/music_import *(rw,no_root_squash)" >> /etc/exports
+    echo "/home/rd/traffic_export *(rw,no_root_squash)" >> /etc/exports
+    echo "/home/rd/traffic_import *(rw,no_root_squash)" >> /etc/exports
+    systemctl enable rpcbind
+    systemctl enable nfs-server
+
+    #
+    # Enable CIFS File Sharing
+    #
+    systemctl enable smb
+    systemctl enable nmb
+fi
+
+if test $MODE = "standalone" ; then
+    #
+    # Install MariaDB
+    #
+    yum -y install mariadb-server
+    systemctl start mariadb
+    systemctl enable mariadb
+    mkdir -p /etc/systemd/system/mariadb.service.d/
+    cp /usr/share/rivendell-install/limits.conf /etc/systemd/system/mariadb.service.d/
+    systemctl daemon-reload
+
+    #
+    # Enable DB Access for localhost
+    #
+    echo "CREATE DATABASE Rivendell;" | mysql -u root
+    echo "CREATE USER 'rduser'@'localhost' IDENTIFIED BY 'letmein';" | mysql -u root
+    echo "GRANT SELECT,INSERT,UPDATE,DELETE,CREATE,DROP,INDEX,ALTER,CREATE TEMPORARY TABLES,LOCK TABLES ON Rivendell.* TO 'rduser'@'localhost';" | mysql -u root
+
+    #
+    # Enable CIFS File Sharing
+    #
+    systemctl enable smb
+    systemctl enable nmb
+fi
+
+#
+# Install Rivendell
+#
+patch -p0 /etc/rsyslog.conf /usr/share/rivendell-install/rsyslog.conf.patch
+cp -f /usr/share/rivendell-install/selinux.config /etc/selinux/config
+systemctl disable firewalld
+yum -y remove chrony openbox
+systemctl start ntpd
+systemctl enable ntpd
+rm -f /etc/asound.conf
+cp /usr/share/rivendell-install/visual.sh /etc/profile.d/
+cp /usr/share/rivendell-install/asihpi.conf /etc/modprobe.d/
+cp /usr/share/rivendell-install/asound.conf /etc/
+cp /usr/share/rivendell-install/*.repo /etc/yum.repos.d/
+cp /usr/share/rivendell-install/RPM-GPG-KEY* /etc/pki/rpm-gpg/
+mkdir -p /usr/share/pixmaps/rivendell
+cp /usr/share/rivendell-install/rdairplay_skin.png /usr/share/pixmaps/rivendell/
+cp /usr/share/rivendell-install/rdpanel_skin.png /usr/share/pixmaps/rivendell/
+mv /etc/samba/smb.conf /etc/samba/smb-original.conf
+cp /usr/share/rivendell-install/smb.conf /etc/samba/
+cp /usr/share/rivendell-install/no_screen_blank.conf /etc/X11/xorg.conf.d/
+mkdir -p /etc/skel/Desktop
+cp /usr/share/rivendell-install/skel/paravel_support.pdf /etc/skel/Desktop/First\ Steps.pdf
+ln -s /usr/share/rivendell/opsguide.pdf /etc/skel/Desktop/Operations\ Guide.pdf
+tar -C /etc/skel -zxf /usr/share/rivendell-install/xfce-config.tgz
+adduser -c Rivendell\ Audio --groups audio,wheel rd
+chown -R rd:rd /home/rd
+chmod 0755 /home/rd
+patch /etc/gdm/custom.conf /usr/share/rivendell-install/autologin.patch
+yum -y remove alsa-firmware alsa-firmware-tools
+yum -y install lame rivendell rivendell-opsguide
+
+if test $MODE = "server" ; then
+    #
+    # Initialize Automounter
+    #
+    cp -f /usr/share/rivendell-install/auto.misc.template /etc/auto.misc
+    systemctl enable autofs
+
+    #
+    # Create Rivendell Database
+    #
+    rddbmgr --create --generate-audio
+    echo "update STATIONS set REPORT_EDITOR_PATH=\"/usr/bin/gedit\"" | mysql -u rduser -pletmein Rivendell
+
+    #
+    # Create common directories
+    #
+    mkdir -p /home/rd/rd_xfer
+    chown rd:rd /home/rd/rd_xfer
+
+    mkdir -p /home/rd/music_export
+    chown rd:rd /home/rd/music_export
+
+    mkdir -p /home/rd/music_import
+    chown rd:rd /home/rd/music_import
+
+    mkdir -p /home/rd/traffic_export
+    chown rd:rd /home/rd/traffic_export
+
+    mkdir -p /home/rd/traffic_import
+    chown rd:rd /home/rd/traffic_import
+fi
+
+if test $MODE = "standalone" ; then
+    #
+    # Initialize Automounter
+    #
+    cp -f /usr/share/rivendell-install/auto.misc.template /etc/auto.misc
+    systemctl enable autofs
+
+    #
+    # Create Rivendell Database
+    #
+    rddbmgr --create --generate-audio
+    echo "update STATIONS set REPORT_EDITOR_PATH=\"/usr/bin/gedit\"" | mysql -u rduser -pletmein Rivendell
+
+    #
+    # Create common directories
+    #
+    mkdir -p /home/rd/rd_xfer
+    chown rd:rd /home/rd/rd_xfer
+
+    mkdir -p /home/rd/music_export
+    chown rd:rd /home/rd/music_export
+
+    mkdir -p /home/rd/music_import
+    chown rd:rd /home/rd/music_import
+
+    mkdir -p /home/rd/traffic_export
+    chown rd:rd /home/rd/traffic_export
+
+    mkdir -p /home/rd/traffic_import
+    chown rd:rd /home/rd/traffic_import
+fi
+
+if test $MODE = "client" ; then
+    #
+    # Initialize Automounter
+    #
+    rm -f /etc/auto.rd.audiostore
+    cat /usr/share/rivendell-install/auto.rd.audiostore.template | sed s/@IP_ADDRESS@/$IP_ADDR/g > /etc/auto.rd.audiostore
+
+    rm -f /home/rd/rd_xfer
+    ln -s /misc/rd_xfer /home/rd/rd_xfer
+    rm -f /home/rd/music_export
+    ln -s /misc/music_export /home/rd/music_export
+    rm -f /home/rd/music_import
+    ln -s /misc/music_import /home/rd/music_import
+    rm -f /home/rd/traffic_export
+    ln -s /misc/traffic_export /home/rd/traffic_export
+    rm -f /home/rd/traffic_import
+    ln -s /misc/traffic_import /home/rd/traffic_import
+    rm -f /etc/auto.misc
+    cat /usr/share/rivendell-install/auto.misc.client_template | sed s/@IP_ADDRESS@/$IP_ADDR/g > /etc/auto.misc
+    systemctl enable autofs
+
+    #
+    # Configure Rivendell
+    #
+    cat /etc/rd.conf | sed s/localhost/$IP_ADDR/g > /etc/rd-temp.conf
+    rm -f /etc/rd.conf
+    mv /etc/rd-temp.conf /etc/rd.conf
+fi
+
+#
+# Finish Up
+#
+echo
+echo "Installation of Rivendell is complete.  Reboot now."
+echo
+echo "IMPORTANT: Be sure to see the FINAL DETAILS section in the instructions"
+echo "           to ensure that your new Rivendell system is properly secured."
+echo
---
+++ repo/install_rivendell.sh
@@ -0,0 +1,122 @@
+#!/bin/bash
+
+# install_rivendell.sh
+#
+# User-facing installation script for Rivendell
+#
+#   (C) Copyright 2021 Fred Gleason <fredg@paravelsystems.com>
+#
+#   This program is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as
+#   published by the Free Software Foundation; either version 2 of
+#   the License, or (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public
+#   License along with this program; if not, write to the Free Software
+#   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+#
+
+function Continue {
+  read -a RESP -p "Continue (y/N) "
+  echo
+  if [ -z $RESP ] ; then
+    exit 0
+  fi
+  if [ $RESP != "y" -a $RESP != "Y" ] ; then
+    exit 0
+  fi
+}
+
+
+function CheckNetwork {
+    ping -c 2 software.paravelsystems.com > /dev/null 2> /dev/null
+    if [ $? != 0 ] ; then
+	echo "Unable to access the public Internet, exiting."
+	exit 1
+    fi
+}
+
+
+function AddRepos {
+    echo "Adding repo..."
+    wget http://software.paravelsystems.com/CentOS/7rd4/RPM-GPG-KEY-Paravel-Broadcast /etc/pki/rpm-gpg/
+    wget http://software.paravelsystems.com/CentOS/7rd4/Paravel-Rivendell4.repo
+    yum -y clean expire-cache
+    yum -y install rhel-rivendell-installer
+}
+
+
+function InstallStandalone {
+    CheckNetwork
+    AddRepos
+    /usr/share/rhel-rivendell-installer/installer_install_rivendell.sh --standalone
+    apt -y remove ubuntu-rivendell-installer
+    exit 0
+}
+
+
+function InstallServer {
+    CheckNetwork
+    AddRepos
+    /usr/share/rhel-rivendell-installer/installer_install_rivendell.sh --server
+    yum -y remove ubuntu-rivendell-installer
+    exit 0
+}
+
+
+function InstallClient {
+    CheckNetwork
+    echo -n "Enter IP address of Rivendell server: "
+    read RD_SERVER
+    AddRepos
+    /usr/share/rhel-rivendell-installer/installer_install_rivendell.sh --client $RD_SERVER
+#    yum -y remove rhel-rivendell-installer
+    exit 0
+}
+
+#
+# Print welcome message and menu
+#
+echo "Welcome to the Rivendell installer!"
+echo
+echo "This installer downloads the Rivendell Radio Automation System from"
+echo "the Internet and installs it on this system."
+echo
+echo "Three different styles of setup for Rivendell are available:"
+echo
+echo " 1) Standalone. All system components (Rivendell code, database server"
+echo "    and audio store) will be installed on this system, allowing it to"
+echo "    function as a self-contained, autonomous Rivendell system."
+echo
+echo " 2) Server. Same as Standalone, but in addition, configure the database"
+echo "    and audio store to be shared with other Rivendell systems over the"
+echo "    network."
+echo
+echo " 3) Client. Install just the Rivendell components, configuring it to"
+echo "    use the database and audio store on a shared server."
+echo
+echo " 4) Do nothing, and exit this installer."
+echo
+read -a RESP -p " Your choice [4]? "
+echo
+
+if [ -z $RESP ] || [ $RESP == "4" ] ; then
+    exit 0
+fi
+if [ $RESP == "1" ] ; then
+    InstallStandalone
+fi
+if [ $RESP == "2" ] ; then
+    InstallServer
+fi
+if [ $RESP == "3" ] ; then
+    InstallClient
+fi
+
+echo "Unrecognized choice: $RESP"
+exit 1
