f34e8686c0ae0c045bb9274ef85bd51fc46e3dcf
Fred Gleason <fredg@paravelsystems.com>
2021-06-08 13:17:50 -0400

    2021-06-07 Fred Gleason <fredg@paravelsystems.com> 	* Updated 'repo/sanity_check.sh' to check os-release(5) for correct 	 distro.
    Signed-off-by: Fred Gleason <fredg@paravelsystems.com>

--- repo/sanity_check.sh
+++ repo/sanity_check.sh
@@ -1,3 +1,14 @@
 #!/bin/sh
 
+. /etc/os-release
+
+if test $ID != "centos" ; then
+    echo "unsupported distro \"$PRETTY_NAME\" detected"
+    exit 1
+fi
+if test $VERSION_ID != "7" ; then
+    echo "unsupported CentOS version \"$VERSION_ID\" detected"
+    exit 1
+fi
+
 exit 0
