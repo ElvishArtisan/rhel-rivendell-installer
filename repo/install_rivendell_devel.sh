fefd71fc20d5c06b7df3ebe9e5433624d8edbb16
Fred Gleason <fredg@paravelsystems.com>
2021-10-12 17:01:38 -0400

    2021-10-12 Fred Gleason <fredg@paravelsystems.com> 	* Updated the package installation list in 	'repo/install_rivendell_devel.sh'. 	* Added code to bootstrap fop(1) to 'repo/install_rivendell_devel.sh'. 	* Updated the 'make clean' repo rule to remove leftover '.sig' files.
    Signed-off-by: Fred Gleason <fredg@paravelsystems.com>

--- repo/install_rivendell_devel.sh
+++ repo/install_rivendell_devel.sh
@@ -24,22 +24,70 @@
 #
 # Enable CodeReady Builder Repo
 #
-subscription-manager repos --enable codeready-builder-for-rhel-8-x86_64-rpms
+# subscription-manager repos --enable codeready-builder-for-rhel-8-x86_64-rpms
 
 #
 # Tag Dependencies
 #
-dnf mark install apr apr-util apr-util-bdb apr-util-openssl cdparanoia-libs hpklinux httpd httpd-filesystem httpd-tools id3lib jack-audio-connection-kit libcoverart libdiscid libffado libxml++ mod_http2 python3-mysql python3-pyserial qt5-qtbase-mysql qt5-qtstyleplugins redhat-logos-httpd
+# dnf mark install apr apr-util apr-util-bdb apr-util-openssl cdparanoia-libs hpklinux httpd httpd-filesystem httpd-tools id3lib jack-audio-connection-kit libcoverart libdiscid libffado libxml++ mod_http2 python3-mysql python3-pyserial qt5-qtbase-mysql qt5-qtstyleplugins redhat-logos-httpd
 
 #
 # Install Packages
 #
-yum -y install git gcc-c++ automake autoconf libtool qt-devel libcurl-devel cdparanoia-devel hpklinux-devel alsa-lib-devel jack-audio-connection-kit-devel libsamplerate-devel libsndfile-devel id3lib-devel libvorbis-devel flac-devel pam-devel soundtouch-devel twolame-devel libmad-devel lame-devel rpm-build createrepo fop docbook5-style-xsl libxslt kernel-devel rpm-sign man-pages openssl-devel taglib-devel libmusicbrainz5-devel libdiscid-devel libcoverart libcoverart-devel
+yum -y install \
+    alsa-lib-devel \
+    autoconf \
+    automake \
+    cdparanoia-devel \
+    createrepo \
+    docbook5-style-xsl \
+    elfutils-libelf-devel \
+    flac-devel \
+    git \
+    gcc-c++ \
+    hpklinux-devel \
+    id3lib-devel \
+    jack-audio-connection-kit-devel \
+    java-1.8.0-openjdk\
+    kernel-devel \
+    kernel-rpm-macros \
+    lame-devel \
+    libcoverart \
+    libcoverart-devel \
+    libcurl-devel \
+    libdiscid-devel \
+    libmad-devel \
+    libmusicbrainz5-devel \
+    libsamplerate-devel \
+    libsndfile-devel \
+    libtool \
+    libvorbis-devel \
+    libxslt \
+    make \
+    man-pages \
+    openssl-devel \
+    pam-devel \
+    qt5-linguist \
+    qt5-qtbase-devel \
+    qt5-qtbase-mysql \
+    rpm-build \
+    rpm-sign \
+    soundtouch-devel \
+    twolame-devel \
+    openssl-devel \
+    taglib-devel
+
+#
+# Install fop(1)
+#
+rm -rf /usr/local/bin/fop-2.6
+tar -C /usr/local/bin -zvxf /usr/share/rhel-rivendell-installer/fop-2.6-bin.tar.gz
 
 #
 # Configure Environment
 #
 echo "DOCBOOK_STYLESHEETS=/usr/share/sgml/docbook/xsl-ns-stylesheets" > /etc/profile.d/docbook5.sh
+echo "PATH=\$PATH:/usr/local/bin/fop-2.6/fop" >> /etc/profile.d/docbook5.sh
 
 #
 # Finish Up
