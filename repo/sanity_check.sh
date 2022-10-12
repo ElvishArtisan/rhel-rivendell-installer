ea5435b898019dcbe869c9c8cf98c98177d1a1f5
Fred Gleason <fredg@paravelsystems.com>
2022-10-12 16:35:14 -0400

    2022-10-12 Fred Gleason <fredg@paravelsystems.com> 	* Fixed a bug in 'repo/sanity_check.sh' that caused the test 	to fail for point versions of RHEL-8 other than '8.4'.
    Signed-off-by: Fred Gleason <fredg@paravelsystems.com>

--- repo/sanity_check.sh
+++ repo/sanity_check.sh
@@ -6,7 +6,11 @@ if test $ID != "rhel" ; then
     echo "unsupported distro \"$PRETTY_NAME\" detected"
     exit 1
 fi
-if test $VERSION_ID != "8.4" ; then
+VER=`echo $VERSION_ID | tr -cd 8`
+if test $VER -eq 88 ; then
+    $VER=8
+fi
+if test $VER != "8" ; then
     echo "unsupported RHEL version \"$VERSION_ID\" detected"
     exit 1
 fi
