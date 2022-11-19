91112df0f74c246244055fc9d24a8aacc5d82cfe
Fred Gleason <fredg@paravelsystems.com>
2022-11-19 10:10:39 -0500

    2022-11-19 Fred Gleason <fredg@paravelsystems.com> 	* Modified rules in 'repo/' to work with RHEL 9.
    Signed-off-by: Fred Gleason <fredg@paravelsystems.com>

--- repo/sanity_check.sh
+++ repo/sanity_check.sh
@@ -2,15 +2,15 @@
 
 . /etc/os-release
 
-if test $ID != "rhel" ; then
+if test $ID != "rocky" ; then
     echo "unsupported distro \"$PRETTY_NAME\" detected"
     exit 1
 fi
-VER=`echo $VERSION_ID | tr -cd 8`
-if test $VER -eq 88 ; then
-    $VER=8
+VER=`echo $VERSION_ID | tr -cd 9`
+if test $VER -eq 99 ; then
+    $VER=9
 fi
-if test $VER != "8" ; then
+if test $VER != "9" ; then
     echo "unsupported RHEL version \"$VERSION_ID\" detected"
     exit 1
 fi
