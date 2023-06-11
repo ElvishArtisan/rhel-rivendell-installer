d822d6d4f03c844040b871c1385f1301ad97e51f
Fred Gleason <fredg@paravelsystems.com>
2023-06-11 16:31:19 -0400

    2023-06-11 Fred Gleason <fredg@paravelsystems.com> 	* Fixed a regression in 'repo/sanity_check.sh' that broke the 	'make sane' target for distro version 8.8.
    Signed-off-by: Fred Gleason <fredg@paravelsystems.com>

--- repo/sanity_check.sh
+++ repo/sanity_check.sh
@@ -8,7 +8,7 @@ if test $ID != "rocky" ; then
 fi
 VER=`echo $VERSION_ID | tr -cd 8`
 if test $VER -eq 88 ; then
-    $VER=8
+    VER=8
 fi
 if test $VER != "8" ; then
     echo "unsupported RHEL version \"$VERSION_ID\" detected"
